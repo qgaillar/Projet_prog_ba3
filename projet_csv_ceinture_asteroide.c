b191f8360a9182a3a497cfa75887adc51767b882
Romeo Rouzier <romeo.rouzier@epfl.ch>
2021-11-12 12:05:07 +0100

    test asteroide mais jecrois ya un pb avec mes valeurs

--- projet_csv_ceinture_asteroide.c
+++ projet_csv_ceinture_asteroide.c
@@ -5,13 +5,19 @@
 #include <time.h>
 #include <math.h>
 
+
+double calc_demi_petit_axe (double demi_grand_axe, double excentricite) {
+	double b = demi_grand_axe*sqrt(1 - pow(excentricite, 2));
+	return b;
+}
+
 void fichierCSV ( char* filename, int centre_x, int centre_y, double demi_grand_axe, double demi_petit_axe) {
 	FILE * file = fopen(filename, "w+");
     for (int i = 0; i < 101; i++) {
 		
-			
-		double coor_x = centre_x + demi_grand_axe*cos(M_PI*i/50);
-		double coor_y = centre_y + demi_petit_axe *sin(M_PI*i/50);
+		int ua = 150.5;	
+		double coor_x = centre_x + 2.5*ua*demi_grand_axe*cos(M_PI*i/50);
+		double coor_y = centre_y + 2.5*ua*demi_petit_axe *sin(M_PI*i/50);
                      
         fprintf(file, "%0.6f,%0.6f", coor_x, coor_y );    
         fprintf(file, "\n");
@@ -23,6 +29,22 @@ void fichierCSV ( char* filename, int centre_x, int centre_y, double demi_grand_
 
 
 int main(int argc, char * argv[]) {
+	srand(time(NULL));
+    double randomDomaine = RAND_MAX + 1.0;
+    
+    int centre_x = 5000;
+    int centre_y = 5000;
+    
+    
+    double demi_grand_axe_random = rand() / randomDomaine * 1;
+    printf("%f\n", demi_grand_axe_random);
+    
+    double excentricite_random = rand() / randomDomaine * 0.5;
+    printf("%f\n", excentricite_random);
+
+    fichierCSV("asteroide.csv" , centre_x, centre_y, demi_grand_axe_random, calc_demi_petit_axe(demi_grand_axe_random, excentricite_random));
+
+    return 0;
 
 
 
